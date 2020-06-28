0df337dca6fee51fbcf329f1fcfe2e08456dd32d
Ben Atkinson <ben.atkinson@energyaustralia.com.au>
2020-06-28 23:17:53 +1000

    (feat) Add response view page

--- src/app/component/raw-response/raw-response.component.spec.ts
+++ src/app/component/raw-response/raw-response.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RawResponseComponent } from './raw-response.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('RawResponseComponent', () => {
   let component: RawResponseComponent;
@@ -9,6 +10,7 @@ describe('RawResponseComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [RawResponseComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   }));
 
--- src/app/component/raw-response/raw-response.component.ts
+++ src/app/component/raw-response/raw-response.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PeopleService } from '../../service/people.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'agl-raw-response',
@@ -6,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./raw-response.component.scss'],
 })
 export class RawResponseComponent implements OnInit {
-  constructor() {}
+  people$: Observable<string> = null;
 
-  ngOnInit(): void {}
+  constructor(private readonly peopleService: PeopleService) {}
+
+  ngOnInit(): void {
+    console.log('init raw');
+    this.people$ = this.peopleService.getPeople().pipe(map((response) => JSON.stringify(response, null, 2)));
+  }
 }
