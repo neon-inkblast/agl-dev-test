a032e10905ded5940a7362e3bead87986f874fe5
Ben Atkinson <ben.atkinson@energyaustralia.com.au>
2020-06-28 23:18:11 +1000

    (feat) Fix basic e2e to verify title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,18 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display "AGL Developer Test" title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('agl-test app is running!');
+    expect(page.getTitleText()).toEqual('AGL Developer Test');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
